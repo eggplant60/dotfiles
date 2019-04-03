6f80cb77c622f5ef7cee3d62ac5d94dc1086754d
eggplant60 <nagoyan240@gmail.com>
2019-04-04 00:40:29 +0900

    [update] スニペット(yasnippet)の導入 http://vdeep.net/emacs-yasnippet

--- init.el
+++ init.el
@@ -110,3 +110,20 @@
 (require 'jedi)
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
+
+;; 2019/04/03 スニペット
+;; http://vdeep.net/emacs-yasnippet
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/dotfiles/mysnippets"
+	"~/dotfiles/yasnippets"
+	))
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+
+(yas-global-mode 1)
